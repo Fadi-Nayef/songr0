8a30714cd457aa19e19ea72fff48550cd7c64c6f
Fadi_Nayef <adraf38@hotmail.com>
2021-07-11 20:00:14 +0300

    lab11AllSet

--- src/main/java/com/songr/songr/Album.java
+++ src/main/java/com/songr/songr/Album.java
@@ -34,4 +34,28 @@ public class Album {
     public String getImageUrl() {
         return imageUrl;
     }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public void setSongCount(int songCount) {
+        this.songCount = songCount;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Album() {
+
+    }
 }
--- src/main/java/com/songr/songr/GreetingController.java
+++ src/main/java/com/songr/songr/GreetingController.java
@@ -24,10 +24,20 @@ public class GreetingController {
 	}
 
 
-	@ResponseBody
-    @GetMapping("/album")
-    public String getAlbum(){
-        ArrayList<T> album = new ArrayList<T>();
-        album.add()
-        return ;
+    @GetMapping("/albums")
+    public String getAlbum(Model model){
+        ArrayList<Album> album = new ArrayList<>();
+
+        var album1= new Album("title1","artist1",2,5,"url1");
+        var album2= new Album("title2","artist2",2,5,"url2");
+        var album3= new Album("title3","artist3",2,5,"url3");
+
+        album.add(album1);
+        album.add(album2);
+        album.add(album3);
+
+        model.addAttribute("album",album);
+
+        return "album";
+    }
 }
